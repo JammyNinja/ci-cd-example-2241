820b540d955024a12c23a0c0d925dd6aeb9c76d8
Louis Auger <jammyninja95@gmail.com>
2026-03-11 10:07:55 +0000

    Integrate live backend API call using requests
    Add functionality to call and display results from a live backend API.

--- app.py
+++ app.py
@@ -2,8 +2,22 @@ import streamlit as st
 
 from api.fast import root
 
+import requests
+
 st.write(root())
 
 st.write('This is obviously very simple, in reality one would rather use this page to display API results more nicely! :)')
 
 st.write("trivial change")
+
+#call live backend, with requests
+
+backend_url = "https://ci-cd-example-2241-1085609219419.europe-west1.run.app"
+
+st.write("calling live backend")
+result = requests.get(backend_url)
+
+output = result.json()
+
+st.write(output)
+
